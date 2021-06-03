d8d4aa8694fa976aa7f94fe526a449cf486c4386
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-06-03 18:58:38 -0500

    Added createIconButton

--- src/ImageButton.ts
+++ src/ImageButton.ts
@@ -38,3 +38,39 @@ export default function createImageButton(
     </button> 
   `;
 }
+
+/**
+ * Creates an image button. Isn't a custom element because how LitElement handles shadow-dom's / and
+ * prevents us from passing and ID to the img from the host component that's using it.
+ * @param onClick Function to be called when the button is pressed
+ * @param iconPath mdi icon path
+ * @param imgId the id to be applied to the img element
+ */
+export function createIconButton(
+  onClick: () => void, iconPath: string, imgId: string,
+): TemplateResult {
+  const styles = css`
+    .image-button {
+      border: initial;
+      outline: none;
+      background-color: initial;
+      align-items: initial;
+      display: initial;
+      box-sizing: initial;
+    }
+
+    .image-button:active {
+      transform: scale(0.8);
+      filter: grayscale(0.8);
+    }
+  `;
+
+  return html`
+    <style>
+      ${styles}
+    </style>
+    <button type="button" @click=${onClick} class="image-button">
+      <ha-svg-icon .path=${iconPath} class=${imgId}></ha-svg-icon>
+    </button> 
+  `;
+}
