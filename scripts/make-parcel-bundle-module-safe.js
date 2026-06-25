35a61b4f676dbf478628d6913a0ba9e202ac3b89
Gannon Prudhomme <gannonprudhomme@gmail.com>
2026-06-24 22:17:09 -0700

    Make Parcel bundle safe for module loading

---
+++ scripts/make-parcel-bundle-module-safe.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const bundlePath = path.resolve(__dirname, '../dist/dcp.js');
+const parcelRuntimeDeclaration = 'var parcelRequire;';
+const bundle = fs.readFileSync(bundlePath, 'utf8');
+
+if (!bundle.startsWith(parcelRuntimeDeclaration)) {
+  fs.writeFileSync(bundlePath, `${parcelRuntimeDeclaration}${bundle}`);
+}
